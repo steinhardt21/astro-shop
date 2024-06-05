2b9e3761807271e58f94e02a1a65a096980d87cb
steinhardt21 <alexandru.enache@protonmail.com>
2024-06-05 16:15:10 +0300

    fix: get correct module

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
